4db1578b279483cd93bdaaf7b3e7d6c4ca14d46e
Roy Contreras Benitez <roy7474@gmail.com>
2023-07-05 18:05:51 -0400

    added scoring system

--- quiz_game.py
+++ quiz_game.py
@@ -8,23 +8,26 @@ if playing != "yes":
     quit()
 
 print('Okay! \n Lets play :)')
-
+score = 0
 #Questions
 answer = input('\n What does CPU stand for? ')
 if answer.lower() == 'central processing unit':
     print('correct!')
+    score += 1
 else:
     print('Incorrect! :(')
 
 answer = input('\n What does GPU stand for? ').lower()
 if answer == 'graphics processing unit':
     print('correct!')
+    score += 1
 else:
     print('Incorrect! :(')
 
 answer = input('\n What does RAM stand for?  ')
 if answer.lower() == 'random access memory':
     print('correct!')
+    score += 1
 else:
     print('Incorrect! :(')
 
@@ -32,9 +35,11 @@ else:
 answer = input('\n What does PSU stand for? ')
 if answer.lower() == 'power supply':
     print('correct!')
+    score += 1
 else:
     print('Incorrect! :()')
 
+print('You Got ' + str(score) + " Questions Correct!")
 #answer = input('\n  ')
 #if answer == ' ':
 #    print('correct!')
